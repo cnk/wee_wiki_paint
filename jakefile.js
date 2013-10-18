f32f89ea9ba9aac8635bf9079a879dd51402bcd2
Cynthia Kiser <cnk@caltech.edu>
2013-10-17 21:22:03 -0700

    set up the JSLint options

--- jakefile.js
+++ jakefile.js
@@ -1,19 +1,38 @@
 /*global desc, task, jake, fail, complete */
-"use strict";
+(function() {
+    
+    "use strict";
 
-task("default", ["lint"]);
+    task("default", ["lint"]);
 
-desc("lint everything by default");
-task("lint", [], function() {
-    var lint = require("./build/lint/lint_runner.js");
+    desc("lint everything by default");
+    task("lint", [], function() {
+        var lint = require("./build/lint/lint_runner.js");
 
-    var files = new jake.FileList();
-    files.include("**/*.js");
-    files.exclude("node_modules");
+        var files = new jake.FileList();
+        files.include("**/*.js");
+        files.exclude("node_modules");
 
-    var options = {
-	node: true
-    };
-
-    lint.validateFileList(files.toArray(), options, {});
-});
+        lint.validateFileList(files.toArray(), nodeLintOptions(), {});
+    });
+    
+    function nodeLintOptions() {
+        return {
+            bitwise: true,
+            curly: false,
+            eqeqeq: true,
+            forin: true,
+            immed: true,
+            latedef: true,
+            newcap: true,
+            noarg: true,
+            noempty: true,
+            nonew: true,
+            regexp: true,
+            undef: true,
+            strict: true,
+            trailing: true,
+            node: true
+        };
+    }
+}());
