37d9000c5171bf2c1ecefa5a7325f00e6651872b
Cynthia Kiser <cnk@caltech.edu>
2013-10-17 20:59:57 -0700

    add JSHint and set up the default jake task to run it

---
+++ jakefile.js
@@ -0,0 +1,19 @@
+/*global desc, task, jake, fail, complete */
+"use strict";
+
+task("default", ["lint"]);
+
+desc("lint everything by default");
+task("lint", [], function() {
+    var lint = require("./build/lint/lint_runner.js");
+
+    var files = new jake.FileList();
+    files.include("**/*.js");
+    files.exclude("node_modules");
+
+    var options = {
+	node: true
+    };
+
+    lint.validateFileList(files.toArray(), options, {});
+});
