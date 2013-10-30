5f674e7796cbc848d96957fee1706641acab66ce
Cynthia Kiser <cnk@caltech.edu>
2013-10-29 23:13:34 -0700

    test runs and logs things - but doesn't yet have any assertions

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -2,8 +2,21 @@
 "use strict";
 
 var server = require("./server.js");
+// Require node's http module so we can use it's testing methods
+var http = require("http");
 
-exports.testServersNumberMethod = function(test) {
-    test.equal(3, server.number(), "number");
-    test.done();
+exports.tearDown = function(finished) {
+    server.stop();
+    finished();
+};
+
+exports.testServerRespondsToGetRequests = function(test) {
+    server.start();
+    http.get('http://localhost:8080/', function(response) {
+	console.log("Got response: " + response.statusCode);
+	response.on("data", function() {});
+	test.done();
+    }).on('error', function(err) {
+	console.log("Got error: " + err.message);
+    });
 };
--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,20 @@
 // wee_wiki_paint/src/server/server.js
 "use strict";
 
-console.log("This is a server.");
+var http = require("http");
+var server;
 
-exports.number = function() {
-    return 3;
+exports.start = function() {
+    console.log("Server started");
+    server = http.createServer();
+    server.on("request", function(request, response) {
+	console.log("Request called");
+	response.end("OK");
+    });
+    server.listen(8080);
+};
+
+exports.stop = function() {
+    console.log("Server stopping");
+    server.close();
 };
