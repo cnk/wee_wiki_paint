b84a5670ea4e90657a20dc386c86e8967d5e0937
Cynthia Kiser <cnk@caltech.edu>
2013-10-29 21:39:28 -0700

    example of using node's http server

---
+++ spikes/node_http/server.js
@@ -0,0 +1,18 @@
+// Play around with Node.js's built in http server
+// run as `node spikes/node_http/server.js`
+
+"use strict";
+
+var http = require("http");
+var server = http.createServer();
+
+server.on("request", function(request, response) {
+    console.log("received request");
+
+    var body = "<h1>body</h1>";
+
+    response.end(body);
+});
+
+server.listen(8888);
+console.log("server listening on port 8888");
