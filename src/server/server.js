6c40d5614a6163f4b69fd86a755f0d1ce5c86627
Cynthia Kiser <cnk@caltech.edu>
2013-11-02 16:37:46 -0700

    remove console logging of each step

--- src/server/server.js
+++ src/server/server.js
@@ -6,16 +6,14 @@ var server;
 
 exports.start = function(portNumber) {
     if (!portNumber) throw new Error("Port number is required to start the server");
-    console.log("Server started");
+
     server = http.createServer();
     server.on("request", function(request, response) {
-        console.log("Request called");
         response.end("Hello World!");
     });
     server.listen(portNumber);
 };
 
 exports.stop = function(callback) {
-    console.log("Server stopping");
     server.close(callback);
 };
