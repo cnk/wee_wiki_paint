bc5d04df10c44dfbc6fa69aad10418330bff9fe5
Cynthia Kiser <cnk@caltech.edu>
2013-11-01 20:35:07 -0700

    Simple hello world server - with status code and response body tests

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -1,22 +1,38 @@
 // wee_wiki_paint/src/server/_server_test.js
 "use strict";
 
+var PORT = 8080;
+var URL = 'http://localhost:' + PORT + '/';
+
 var server = require("./server.js");
 // Require node's http module so we can use it's testing methods
 var http = require("http");
+
+exports.setUp = function(callback) {
+    server.start(PORT);
+    callback();
+};
 
 exports.tearDown = function(finished) {
     server.stop();
     finished();
 };
 
-exports.testServerRespondsToGetRequests = function(test) {
-    server.start();
-    http.get('http://localhost:8080/', function(response) {
-	console.log("Got response: " + response.statusCode);
-	response.on("data", function() {});
-	test.done();
-    }).on('error', function(err) {
-	console.log("Got error: " + err.message);
+exports.test_serverRespondsToGetRequests = function(test) {
+    http.get(URL, function(response) {
+        test.equals(200, response.statusCode, 'status code should be 200');
+        response.on("data", function() {});
+        test.done();
+    });
+};
+
+exports.test_serverReturnsHelloWorld = function(test) {
+    var request = http.get(URL);
+    request.on("response", function(response) {
+        response.setEncoding('utf8');
+        response.on("data", function(chunk) {
+            test.equals("Hello World!", chunk, "response body contains hello world");
+        });
+        test.done();
     });
 };
