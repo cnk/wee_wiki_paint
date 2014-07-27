3124cbea6bf6760cbeb062b72c0b724a6b32cab6
Cynthia Kiser <cnk@caltech.edu>
2014-07-26 22:40:36 -0700

    add Raphael in vendor

--- karma.conf.js
+++ karma.conf.js
@@ -16,6 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
         "vendor/client/jquery-1.11.0.js",
+        "vendor/client/raphael-2.1.2.js",
         "src/client/*.js"
     ],
 
