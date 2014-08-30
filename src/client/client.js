628229ccca588146c17c7923100bb75a43158164
Cynthia Kiser <cnk@caltech.edu>
2014-08-30 15:27:41 -0700

    draw line with mouse clicks

--- src/client/client.js
+++ src/client/client.js
@@ -12,13 +12,18 @@ wwp = {};
         paper = new Raphael(drawingAreaId);
         // spike 8/30/14
         var drawingArea = $('#'+drawingAreaId);
-        drawingArea.click(function(event) {
-            var divX = drawingArea.offset().left;
-            var divY = drawingArea.offset().top;
+        var divX = drawingArea.offset().left;
+        var divY = drawingArea.offset().top;
 
+        var previousX = 0;
+        var previousY = 0;
+        drawingArea.click(function(event) {
             var relativeX = event.pageX - divX;
             var relativeY = event.pageY - divY;
             paper.circle(relativeX, relativeY, 2).attr("fill", "red");
+            wwp.drawLine(previousX, previousY, relativeX, relativeY);
+            previousX = relativeX;
+            previousY = relativeY;
         });
         // end spike
 
