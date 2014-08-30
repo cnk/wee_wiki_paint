93bbb0d6317555d2364c6b6efb3bb2f2919a593a
Cynthia Kiser <cnk@caltech.edu>
2014-08-30 11:52:41 -0700

    clean up tests just a little

--- src/client/_client_test.js
+++ src/client/_client_test.js
@@ -6,11 +6,13 @@
 
     describe('Drawing area', function() {
         var drawingDiv = $('<div id="drawingArea" style="width: 600px; height: 400px;">drawing goes here</div>');
+        var paper;
 
         beforeEach(function() {
             // Create the div that will be assumed to be on our web page in production
             // In other words, set up a mock for the web page
             $(document.body).append(drawingDiv);
+            paper = wwp.initializeDrawingArea('drawingArea');
         });
 
         afterEach(function() {
@@ -18,32 +20,28 @@
         });
 
         it("should be initialized in a predefined div", function() {
-            // Initialize it with WWP fuctions
-            wwp.initializeDrawingArea('drawingArea');
-
             // Test the div is set up
             var tagName = $('div#drawingArea').children()[0].tagName;
             expect(tagName).to.equal('svg');
         });
 
         it("the Raphael canvas should be contained within the drawing area", function() {
-            var paper = wwp.initializeDrawingArea('drawingArea');
-
             expect(paper.width).to.equal(600);
             expect(paper.height).to.equal(400);
         });
 
         it("should draw a line", function() {
-            var paper = wwp.initializeDrawingArea('drawingArea');
             var startX = 20; var startY = 200; var endX = 200; var endY = 400;
             wwp.drawLine(startX, startY, endX, endY);
 
+            // DrawLine added a single element to the paper
             var elements = [];
             paper.forEach(function(element) {
                 elements.push(element);
             });
             expect(elements.length).to.equal(1);
 
+            // The new element is a path with the expected attributes
             var line = elements[0].node.attributes.d.value;
             var expected = ['M', startX, ',', startY, 'L', endX, ',', endY].join('');
             expect(line).to.equal(expected);
